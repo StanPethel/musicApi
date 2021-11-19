81a965a3d0e31305fe8aa4766784cdbc23dd4ea0
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-19 16:55:20 -0500

    updated/added/finalizedfiles

---
+++ middleWare/songs-validation.js
@@ -0,0 +1,14 @@
+exports.validateSong = (req, res, next) => {
+    const data = req.body;
+    if(
+        (data.hasOwnProperty('id') && typeof(data.id) === 'string') &&
+        (data.hasOwnProperty('title') && typeof(data.title) === 'string') &&
+        (data.hasOwnProperty('album') && typeof(data.album) === 'string') &&
+        (data.hasOwnProperty('genre') && typeof(data.genre) === 'string') &&
+        (data.hasOwnProperty('releaseDate') && typeof(data.releaseDate) === 'number')
+    ){
+        return next();
+    }else{
+        return res.status(400).send({error: 'Missing required properties.'});
+    }
+}
